4a3a8fe57e1774076d54258c6ba2bb33157364f5
haiddph <haiddph20767@fpt.edu.vn>
2023-07-18 15:45:00 +0700

    fix app.js

--- Be/src/app.js
+++ Be/src/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import RouterCategories from "./routes/category";
+import RouterProducts from "./routes/product";
 
 // Config
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 // connect
 app.use("/api", RouterCategories);
+app.use("/api", RouterProducts);
 mongoose
     .connect("mongodb://127.0.0.1:27017/nodejs-React")
     .then(() => {
